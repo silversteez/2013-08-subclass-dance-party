61561455f49bb38a9dbe59507a4cf38f40886ca9
Hack Reactor Students <github@hackreactor.com>
2013-08-12 20:13:04 -0700

    Added snoopDancer

---
+++ src/snoopDancer.js
@@ -0,0 +1,13 @@
+var SnoopDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+};
+
+SnoopDancer.prototype = Object.create(Dancer.prototype);
+SnoopDancer.prototype.constructor = SnoopDancer;
+
+SnoopDancer.prototype.step = function(){
+//  Dancer.prototype.step.call(this);
+  this.$node.append('<img src="snoop.gif"/>');
+};
